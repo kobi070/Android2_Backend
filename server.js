e5dc71bfb436ac192a2864cce9dca2696459e2db
kobi070 <kobi070@gmail.com>
2023-07-02 20:10:08 +0300

    API v1.0 - Server.js updated

--- server.js
+++ server.js
@@ -19,24 +19,6 @@ connectToDB().then((connection) => {
   console.log("Connecting to MongoDB");
 }).catch((error) => console.log(`Error: ${error}`));
 
-// MongoDB Atlas connection URI
-// const { MONGO_URI } = process.env;
-// // Connect to MongoDB Atlas
-// mongoose
-//   .connect(MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => {
-//     console.log("Connected to MongoDB Atlas");
-//     // Load data into the database only once
-//     api.fetchAndInsertData();
-//     // db.populateDatabase();
-//   })
-//   .catch((err) => {
-//     console.error("Failed to connect to MongoDB Atlas", err);
-//   });
-
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
