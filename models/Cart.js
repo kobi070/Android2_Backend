cd04681cdaa62cd98ed1c06a0c029070bb8377f4
kobi070 <kobi070@gmail.com>
2023-07-04 16:09:45 +0300

    API v1.0 - Cart.js updated to Refrence User and Product Schema

--- models/Cart.js
+++ models/Cart.js
@@ -2,12 +2,22 @@ const mongoose = require("mongoose");
 
 const cartSchema = new mongoose.Schema({
   id: Number,
-  user_id: Number,
+  user_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    localField: "user_id", // Specify the local field to reference in the current schema
+    foreignField: "id", // Specify the foreign field in the referenced schema
+  },
   date: Date,
   products: [
     {
-      productId:Number,
-      quantity:Number
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+        localField: "productId", // Specify the local field to reference in the current schema
+        foreignField: "id", // Specify the foreign field in the referenced schema
+      },
+      quantity: Number,
     }
   ],
 });
