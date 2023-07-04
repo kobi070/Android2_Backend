95075fa5ef0a3fa1438c14cd79d27a3c7bdfa03c
kobi070 <kobi070@gmail.com>
2023-07-04 15:26:22 +0300

    API v1.0 - Category.js Updated

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,16 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const categorySchema = mongoose.Schema({
+const categorySchema = mongoose.Schema(
+  {
+    title: String,
+    description: {
+      type: String,
+      default: function() {
+        return this.title;
+      },
+    },
+  },
+  { timestamps: true }
+);
 
-    title : String,
-    image : {type : String, default : null},
-    icon : {type : String, default : null},
-    description : {type : String, default : null},
-
-}, { timestamps : true })
-
-module.exports = mongoose.model("categories", categorySchema)
+module.exports = mongoose.model("categories", categorySchema);
