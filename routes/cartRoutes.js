2a31b68506fa2a8fd868f964be063265bdff83db
kobi070 <kobi070@gmail.com>
2023-07-05 13:31:03 +0300

    API v1.0 - cartRoutes.js updated

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const cartController = require("../controllers/user/CartController");
+const cartController = require("../controllers/cart/CartController");
 
 // Get all carts
 router.get("/", cartController.getAllCarts);
