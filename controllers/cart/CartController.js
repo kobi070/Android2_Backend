03524f5f10db64179e946bd61972937a0096e412
kobi070 <kobi070@gmail.com>
2023-07-09 13:29:31 +0300

    API v1.0 - fix

--- controllers/cart/CartController.js
+++ controllers/cart/CartController.js
@@ -1,5 +1,6 @@
 const Cart = require("../../models/Cart");
 const User = require("../../models/User");
+const Product = require("../../models/Product");
 
 // Get all carts
 exports.getAllCarts = async (req, res) => {
